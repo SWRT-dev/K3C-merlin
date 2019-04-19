9f4ca3bd7177dc09ee70c679ec22cd6195070c54
paldier <paldier@hotmail.com>
2019-04-19 22:00:15 +0800

    shared: merged merlin run_custom_script() and run_custom_script_blocking()

--- release/src/router/rc/init.c
+++ release/src/router/rc/init.c
@@ -10564,7 +10564,7 @@ int init_main(int argc, char *argv[])
 #ifdef RTN65U
 		asm1042_upgrade(1);	// check whether upgrade firmware of ASM1042
 #endif
-		run_custom_script("init-start", NULL);
+		run_custom_script("init-start", 0, NULL, NULL);
 
 		state = SIGUSR2;	/* START */
 
